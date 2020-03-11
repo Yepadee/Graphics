04207a3302e86b9d36cda7d9841a3778f0fd5546
Yepadee <james@hawkinsplace.co.uk>
2020-03-11 13:13:49 +0000

    replaced light source calculation with value

--- src/graphics.cpp
+++ src/graphics.cpp
@@ -46,15 +46,9 @@ float focalLength = WIDTH / 2;
 
 std::vector<Object> objects = loadOBJ("models/cornell-box.obj", 1.0f);
 
-vec3 a (-0.884011, 4.718497, -3.567968);
-vec3 b (0.415989, 4.719334, -2.517968);
-vec3 light = a + ((glm::length(a - b) / 3) * -(a - b));
-
-vec4 lightSource(light, 100.0f);
-
 std::vector<vec4> lights = {
-  lightSource
-  //, {0.0f, 2.4f, 3.8f, 100.0f}
+    {-0.159877f, 4.71896f, -2.98309f, 150.0f}
+  , {0.0f, 2.4f, 3.8f, 100.0f}
 }; 
 
 int drawMode = 2;
